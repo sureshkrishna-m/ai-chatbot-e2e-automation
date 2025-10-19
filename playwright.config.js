28791e6ddc06cc6cc9026d28cb5c3b5387196f87
SureshKrishna M <sureshkrishnam@SureshKrishnas-MacBook-Pro.local>
2025-10-19 22:17:35 +0530

    Allure yml file updates

--- playwright.config.js
+++ playwright.config.js
@@ -15,9 +15,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? undefined : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
     ? [['html', { open: 'never' }], ['allure-playwright', { outputFolder: 'allure-results' }]]
